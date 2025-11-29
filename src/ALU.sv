116b87d75c479c376e1faa6b08c47c5798a45d34
Dayana Rios <korotz10@gmail.com>
2025-11-28 20:44:40 -0500

    Fix SLL, SRL and SRA instruccions

--- src/ALU.sv
+++ src/ALU.sv
@@ -4,7 +4,7 @@ module ALU(
 
     input logic [3:0] ALUOp,
 
-    output logic signed [31:0] ALURes = 0
+    output logic signed [31:0] ALURes
 );
 
     // It executes each time any input changes
@@ -12,15 +12,16 @@ module ALU(
         case(ALUOp)
             4'b0000: ALURes = A + B; // ADD
             4'b1000: ALURes = A - B; // SUB
-            4'b0001: ALURes = A << B; // SLL
+            4'b0001: ALURes = A << B[4:0]; // SLL
             4'b0010: ALURes = A < B; // SLT
             4'b0011: ALURes = $unsigned(A) < $unsigned(B); // SLTU
             4'b0100: ALURes = A ^ B; // XOR
-            4'b0101: ALURes = A >> B; // SRL
-            4'b1101: ALURes = A >>> B; // SRA
+            4'b0101: ALURes = A >> B[4:0]; // SRL
+            4'b1101: ALURes = A >>> B[4:0]; // SRA
             4'b0110: ALURes = A | B; // OR
             4'b0111: ALURes = A & B; // AND
             4'b1001: ALURes = B;
+            default: ALURes = 32'd0; // Default case
         endcase
     end
 endmodule
